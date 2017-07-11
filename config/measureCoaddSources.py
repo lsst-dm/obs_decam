9a14680ae61b6b883743e717aeb8ea7e97652cc3
ih64 <imran.hasan.ih@gmail.com>
2017-07-11 15:25:41 -0700

    added configs for cmodel and for measurecoaddsources.py

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -1,7 +1,13 @@
+"""obs mosiac specific overrides for MeasureMergedCoaddSourcesTask"""
+
 import os.path
 from lsst.utils import getPackageDir
 from lsst.meas.extensions.astrometryNet import LoadAstrometryNetObjectsTask
 
+#configs to use the astrometry refobjloader in directMatch
 config.match.refObjLoader.retarget(LoadAstrometryNetObjectsTask)
 config.match.refObjLoader.load(os.path.join(getPackageDir("obs_mosaic"), "config", "filterMap.py"))
 config.match.refObjLoader.defaultFilter = 'r'
+
+#adds jims cmodel as a plugin to measurement
+config.load(os.path.join(getPackageDir("obs_mosaic"), "config", "cmodel.py"))
